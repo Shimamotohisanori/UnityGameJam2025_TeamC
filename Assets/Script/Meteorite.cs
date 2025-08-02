d71805d32a4b84878c85d8f4c9ac71242d41f0f7
iwamotorikuto <ca01254003@st.kawahara.ac.jp>
2025-08-02 12:10:30 +0900

    <Map>Meteoの動作の追加

--- Assets/Script/Meteorite.cs
+++ Assets/Script/Meteorite.cs
@@ -6,7 +6,7 @@ public class Meteo : MonoBehaviour
 {
     public GameObject ball;
     private float count = 1.0f;
-    private int vecX;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -20,9 +20,8 @@ public class Meteo : MonoBehaviour
         count -= Time.deltaTime;
         if(count <= 0)
         {
-            vecX = Random.Range(-9, 10);
-            Instantiate(ball, new Vector3(vecX, 8, 0), Quaternion.identity);
-            count = 0;
+            Instantiate(ball, new Vector3(10, 16, 10), Quaternion.identity);
+            count = 1.0f;
         }
     }
 }
--- Assets/Script/metoriteDelte.cs
+++ Assets/Script/metoriteDelte.cs
@@ -13,6 +13,9 @@ public class metoriteDelte : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(this.transform.position.y < -3)
+        {
+            Destroy(gameObject);
+        }
     }
 }
