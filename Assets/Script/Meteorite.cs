71676fcf64889a303fe228bef9948c1b4c7dcd23
iwamotorikuto <ca01254003@st.kawahara.ac.jp>
2025-08-02 10:43:39 +0900

    <Map>MeteoriteDelteのScriptの追加

--- Assets/Script/Meteorite.cs
+++ Assets/Script/Meteorite.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Meteo : MonoBehaviour
 {
+    public GameObject ball;
+    private float count = 1.0f;
+    private int vecX;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,12 @@ public class Meteo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        count -= Time.deltaTime;
+        if(count <= 0)
+        {
+            vecX = Random.Range(-9, 10);
+            Instantiate(ball, new Vector3(vecX, 8, 0), Quaternion.identity);
+            count = 0;
+        }
     }
 }
