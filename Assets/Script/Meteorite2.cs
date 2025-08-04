03bca0d1de612efc3e97b92ee35fd526458e03b2
iwamotorikuto <ca01254003@st.kawahara.ac.jp>
2025-08-04 09:50:26 +0900

    <Map>ｍeteoDelte2、meteoDelte3、Meteorite2、Meteorite3の追加

---
+++ Assets/Script/Meteorite2.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteorite2 : MonoBehaviour
+{
+    public GameObject ball;
+    private float count = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        count -= Time.deltaTime;
+        if (count <= 0)
+        {
+            Instantiate(ball, new Vector3(5, 11, 5), Quaternion.identity);
+            count = 1.0f;
+        }
+    }
+}
+
