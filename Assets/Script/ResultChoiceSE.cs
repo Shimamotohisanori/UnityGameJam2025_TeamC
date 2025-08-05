f96e5fdcf5d984c4b37fa700bd304baea7abe43d
kimurahayato-alpha <kimurahayato0102@gmail.com>
2025-08-05 15:59:25 +0900

    create sound (#14)
    Co-authored-by: KawaTaro <ca01254006@st.kawahara.ac.jp>

---
+++ Assets/Script/ResultChoiceSE.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultChoiceSE : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public AudioClip resultchoicese;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            audioSource.PlayOneShot(resultchoicese);
+        }
+    }
+}
