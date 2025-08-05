91b9bbe45745fa0fd727187baca8d3fc38589e9c
Shimamoto Hisanori <ca01254009@st.kawahara.ac.jp>
2025-08-05 09:01:52 +0900

    プレイヤーの残機が減ったらリザルト画面に飛ぶようになった

--- Assets/Script/PlayerHP.cs
+++ Assets/Script/PlayerHP.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHP : MonoBehaviour
 {
@@ -32,6 +33,7 @@ public class PlayerHP : MonoBehaviour
             lifeText.text = "Life: " + life;//ライフを表示する
             if (life < 1)
             {
+                SceneManager.LoadScene("ResultScene");//ライフが0以下になったらリザルトシーンに移動
                 Destroy(gameObject);//プレイヤーが小惑星に当たったらプレイヤーを削除
             }
         }
