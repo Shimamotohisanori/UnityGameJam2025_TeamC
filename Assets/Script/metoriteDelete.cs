cdc79c82d31727e55aca7c542385ae2446238c65
iwamotorikuto <ca01254003@st.kawahara.ac.jp>
2025-08-08 20:32:51 +0900

    隕石のDelete間隔の削除

--- Assets/Script/metoriteDelete.cs
+++ Assets/Script/metoriteDelete.cs
@@ -13,9 +13,9 @@ public class metoriteDelte : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (this.transform.position.y < -9)
-        {
-            Destroy(gameObject);
-        }
+        //if (this.transform.position.y < -9)
+        //{
+        //    Destroy(gameObject);
+        //}
     }
 }
